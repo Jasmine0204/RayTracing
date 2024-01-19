631785dbe90dabc943298f7b98054f16e5d3a73e
Jasmine Chen <70587225+Jasmine0204@users.noreply.github.com>
2024-01-19 12:59:13 -0500

    1

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -13,21 +13,30 @@ int main() {
 
 	hittable_list world;
 
-	auto material_ground = make_shared<lambertian>(color(0.6,0.5,0.7));
-	auto material_center = make_shared<lambertian>(color(0.7,0.4,1.0));
-	auto material_left = make_shared<metal>(color(0.7,0.7,1.0), 0.3);
-	auto material_right = make_shared<metal>(color(1.0,0.7,0.9), 0.9);
+	auto material_ground = make_shared<lambertian>(color(0.55, 0.82, 0.63));
+	auto material_center = make_shared<lambertian>(color(0.4, 0.96, 0.76));
+	auto material_left = make_shared<dielectric>(1.5);
+	auto material_right = make_shared<metal>(color(0.62, 1, 1), 0.1);
 
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, material_center));
-	world.add(make_shared<sphere>(point3(-1.0, 0, -1), 0.5, material_left));
-	world.add(make_shared<sphere>(point3(1.0, 0, -1), 0.5, material_right));
-	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	camera cam;
 	cam.aspect_ratio = 16.0 / 9.0;
 	cam.image_width = 1600;
 	cam.samples_per_pixel = 10;
 	cam.max_depth = 10;
+
+	cam.vfov = 20;
+	cam.lookfrom = point3(-2, 2, 1);
+	cam.lookat = point3(0, 0, -1);
+	cam.vup = vec3(0, 1, 0);
+
+	cam.defocus_angle = 6.0;
+	cam.focus_dist = 3.4;
 
 	cam.render(world);
 }
